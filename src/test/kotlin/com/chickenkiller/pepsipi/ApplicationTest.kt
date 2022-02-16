7702f9711d7cf89ea4c7de3041b81972761b5e42
96Octavian <gogol.logog@hotmail.it>
2022-02-16 17:32:32 +0100

    Initial commit

---
+++ src/test/kotlin/com/chickenkiller/pepsipi/ApplicationTest.kt
@@ -0,0 +1,31 @@
+package com.chickenkiller.pepsipi
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.locations.*
+import io.ktor.server.plugins.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.chickenkiller.pepsipi.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
